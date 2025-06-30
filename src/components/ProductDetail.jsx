78330968bfd4604ff3e834f87b6dcbcc71a6df57
Taylor Putman <fishputman@gmail.com>
2025-06-30 13:34:34 -0400

    created product detail page

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,9 +1,54 @@
-import { useState } from "react";
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import { useParams } from "react-router-dom";
 
-const ProductDetail = () => {
-  const [value, setValue] = useState("");
+const ProductDetail = ({ products }) => {
+  const { productId } = useParams();
+  const product = products.find((product) => product.id === Number(productId));
 
-  return <div>Hello</div>;
+  if (!product) {
+    return (
+      <div
+        style={{ height: "700px" }}
+        className="d-flex justify-content-center align-items-center "
+      >
+        <h1>Product not found..</h1>
+      </div>
+    );
+  }
+
+  const stars = [];
+  const rating = Math.round(product.rating.rate);
+  for (let i = 1; i <= 5; i++) {
+    stars.push(i <= rating ? "★" : "☆");
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col className="col-4">
+          <img
+            className="w-100 p-3 border border-2 rounded"
+            src={product.image}
+            alt={product.title}
+          />
+        </Col>
+        <Col className="col-8">
+          <h2>{product.title}</h2>
+          <h4>Price: ${product.price}</h4>
+          <div>
+            <span style={{ fontSize: "1.5rem", color: "#FFD700" }}>
+              {stars.join("")}
+            </span>{" "}
+            <span className="fs-5">({product.rating.count})</span>
+          </div>
+
+          <p>{product.description}</p>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default ProductDetail;
